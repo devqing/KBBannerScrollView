9333915141ab7035acb524d9e9464b9315eed095
liuweiqing <liuweiqing@kuaikuaiyu.com>
2016-05-24 11:23:02 +0800

    Initial Commit

---
+++ KBLoopBannerViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KBLoopBannerViewDemo
+//
+//  Created by liuweiqing on 16/5/24.
+//  Copyright © 2016年 Kobe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KBLoopBannerViewDemo/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  KBLoopBannerViewDemo
+//
+//  Created by liuweiqing on 16/5/24.
+//  Copyright © 2016年 Kobe. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
